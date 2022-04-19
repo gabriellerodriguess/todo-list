c004c7e39c7bc24f90bbe8507277b7e96cae5299
Gabrielle Rodrigues <gabriellerut@gmail.com>
2022-04-19 18:54:53 -0300

    Armazenando no localStorage os dados.

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
+const inputText = document.querySelector('#inputText')
 const todoList = document.querySelector('#todoList')
 
+const getLocalStorage = () => JSON.parse(localStorage.getItem('todoList')) ?? []
+const setLocalStorage = (database) => localStorage.setItem('todoList', JSON.stringify(database))
 
-let dataBase = [
-    {'tarefa':'Estudar'},
-]
 
-const createElement = (tarefa, indice) =>{
+const createElement = (task, index) =>{
     let listItem = `
-    <li data-indice=${indice}>${tarefa}
-        <span data-indice=${indice}>
+    <li data-index=${index}>${task}
+        <span data-index=${index} id='span'>
             <i class="fa-solid fa-trash-can"></i>
         </span>
     </li>
@@ -17,36 +17,56 @@ const createElement = (tarefa, indice) =>{
 }
 
 const insertList = () => {
-        let inputText = document.querySelector('#inputText').value
-        if(inputText.length != 0){
-            dataBase.push({'tarefa': `${inputText}`})
+        if(inputText.value.length != 0){
+            const dataBase = getLocalStorage()
+            dataBase.push({'task': `${inputText.value}`})
+            setLocalStorage(dataBase)
             updateList()  
         } else{
             alert('Digite uma tarefa.') //criar um snackbar para aparecer o erro
-        }
-       
+        }    
+        clearInput()
 }
 
-const clearItemList = () => {
+const clearInput = () => {
+    inputText.value = ''
+}
+
+const clearLastChildList = () => {
     while(todoList.firstChild){
         todoList.removeChild(todoList.lastChild)
     }
 }
 
-const clearAllList = () => {
-    todoList.innerHTML = ''
+const clearAllList = (index) => {
+    const dataBase = getLocalStorage()
+    dataBase.splice(index)
+    setLocalStorage(dataBase)
+    updateList()
 }
 
 const updateList = () => {
-    clearItemList()
-    dataBase.forEach(( item,indice) => createElement(item.tarefa, indice)) //o banco está sendo lido e passado para a função createElement
+    clearLastChildList()
+    const dataBase = getLocalStorage()
+    dataBase.forEach((item,index) => createElement(item.task, index)) //o banco está sendo lido e passado para a função createElement
 }
 
-const clickItem = (evento) => {
-    const elemento = evento.target
-    console.log(elemento)
+const removeItemList = (index) => {
+    const dataBase = getLocalStorage()
+    dataBase.splice(index,1)
+    setLocalStorage(dataBase)
+    updateList()
 }
 
-todoList.addEventListener('click', clickItem)
+const iconRemove = (event) => {
+    const element = event.target
+    if(element.id === 'span'){
+       const index = element.dataset.index 
+        removeItemList(index)
+    }  
+}
+
+
+todoList.onclick = iconRemove
 document.querySelector('#btnAdd').addEventListener('click', () => insertList())
 document.querySelector('#btnClear').addEventListener('click', () => clearAllList())
